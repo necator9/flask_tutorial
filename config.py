e68a9f33c657d1f09171b513febd8ee8b005a4d8
Ivan Matveev <ivan.matveev@hs-anhalt.de>
2020-12-01 08:30:04 +0100

    Start learning DB

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY') or 'custom_and_secure_string'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///{}'.format(os.path.join(basedir, 'app.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
